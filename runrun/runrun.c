57e88747fe39cd3399ab9480d51825a494c4790d
emeath <matheus.martins.jeronimo@gmail.com>
2022-09-04 09:25:24 -0300

    Allocates dynamically a matrix

--- runrun/runrun.c
+++ runrun/runrun.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char** map; 
+int lines;
+int columns;
+
 int main() {
-	char map[5][10+1]; // +1 -> room for '\0' at the end of string
 
 	FILE* f;
 	f = fopen("map.txt", "r");
@@ -10,14 +13,27 @@ int main() {
 		printf("Error reading the map file\n");
 		exit(1);
 	}
+	
+	fscanf(f, "%d %d", &lines, &columns);
+	printf("number of lines %d, number of columns %d\n", lines, columns);
 
-	for(int i = 0; i < 5; i++) {
+	map = malloc(sizeof(char*) * lines);
+	for(int i = 0; i < lines; i++) {
+		map[i] = malloc(sizeof(char) * (columns + 1)); // +1 -> room for '\0' at the end of string
+	}
+
+	for(int i = 0; i < lines; i++) {
 		fscanf(f, "%s", map[i]);
 	}
-	for(int i = 0; i < 5; i++) {
+	for(int i = 0; i < lines; i++) {
 		printf("%s\n", map[i]);
 	}
 	fclose(f);
+	
+	for(int i = 0; i < lines; i++) {
+		free(map[i]);
+	}
+	free(map);
 
 	return 0;
 }
