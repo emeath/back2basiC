f611053ac02fe9d4dcb1e7cc4052d86c64b3a533
emeath <matheus.martins.jeronimo@gmail.com>
2022-09-04 08:58:23 -0300

    Reads from file and prints on screen

--- runrun/runrun.c
+++ runrun/runrun.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-	char map[5][10];
+	char map[5][10+1]; // +1 -> room for '\0' at the end of string
 
-	map[0][0] = '|';
-	map[4][9] = '@';
+	FILE* f;
+	f = fopen("map.txt", "r");
+	if(f == 0) {
+		printf("Error reading the map file\n");
+		exit(1);
+	}
 
-	printf("%c %c\n", map[0][0], map[4][9]);
+	for(int i = 0; i < 5; i++) {
+		fscanf(f, "%s", map[i]);
+	}
+	for(int i = 0; i < 5; i++) {
+		printf("%s\n", map[i]);
+	}
+	fclose(f);
 
+	return 0;
 }
